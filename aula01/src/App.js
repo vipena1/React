39be6d47e8add2d8bece16cf4634b5f950b53e06
vipena1 <viniciuspena81@gmail.com>
2023-02-16 20:52:20 -0300

    import react and class

--- aula01/src/App.js
+++ aula01/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react' //Importação da biblioteca do React para o arquivo
+import Lista from "./Componentes/ListaAlunos"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+  export default function App(){         //Função com o nome do arquivo
+    
+    const aluno = "Vinicius Pena"
+    const curso = "ADS"
+
+    return(               //A função debe retornar o conteudo que será exibido
+      <>
+        <h1>TDS FIAP - RWD </h1>
+        <p>Aluno: {aluno}</p>
+        <p>curso: {curso}</p> 
+        <Lista/>          {/* Chamando a lista criada e importada */}
+      </>
+    )
+  }
---
+++ aula01/src/Componentes/ListaAlunos.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function ListaAlunos(){
+    
+    return(
+        <ul>
+            <li>Huguinho</li>
+            <li>Zézinho</li>
+            <li>Luizinho</li>
+        </ul>
+    )
+}
